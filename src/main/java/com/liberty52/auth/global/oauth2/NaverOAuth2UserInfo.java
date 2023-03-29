baf75127b75c1d04ee8101ae9b7f140415b047f4
sungbin <sungbin@vessl.ai>
2023-03-30 05:18:30 +0900

    Impl naver oauth

---
+++ src/main/java/com/liberty52/auth/global/oauth2/NaverOAuth2UserInfo.java
@@ -0,0 +1,55 @@
+package com.liberty52.auth.global.oauth2;
+
+
+import java.util.Map;
+
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+
+  public NaverOAuth2UserInfo(Map<String, Object> attributes) {
+    super(attributes);
+  }
+
+  @Override
+  public String getId() {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    if (response == null) {
+      return null;
+    }
+    return (String) response.get("id");
+  }
+
+  @Override
+  public String getPhoneNumber() {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    if (response == null) {
+      return null;
+    }
+
+    return (String) response.get("mobile");
+  }
+
+  @Override
+  public String getImageUrl() {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    if (response == null) {
+      return null;
+    }
+
+    return (String) response.get("profile_image");
+  }
+
+  @Override
+  public String getEmail() {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    if (response == null) {
+      return null;
+    }
+
+    return (String) response.get("email");
+  }
+}
+
