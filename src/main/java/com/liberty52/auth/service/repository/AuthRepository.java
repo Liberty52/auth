a2b60bb4cbe87bac1c3eb702ecdd37793e1447b3
sungbin <sungbin@vessl.ai>
2023-03-30 05:17:00 +0900

    Add repository

--- src/main/java/com/liberty52/auth/service/repository/AuthRepository.java
+++ src/main/java/com/liberty52/auth/service/repository/AuthRepository.java
@@ -1,10 +1,12 @@
 package com.liberty52.auth.service.repository;
 
 import com.liberty52.auth.service.entity.Auth;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface AuthRepository extends JpaRepository<Auth, Long> {
 
-public interface AuthRepository extends JpaRepository<Auth, String> {
-    Optional<Auth> findByEmail(String email);
+  Optional<Auth> findByEmail(String email);
+  Optional<Auth> findByRefreshToken(String refreshToken);
 }
+
