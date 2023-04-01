96875d18c70eec95ef1da8a8104c6133ceedfa50
jinlee0 <wls4370@gmail.com>
2023-04-01 14:34:53 +0900

    LIB-32 refactor AuthErrorCode & ..Exception
    AuthWithSuchEmailAlreadyExistsException

--- src/main/java/com/liberty52/auth/global/exception/AuthErrorCode.java
+++ src/main/java/com/liberty52/auth/global/exception/AuthErrorCode.java
@@ -7,15 +7,21 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum AuthErrorCode implements ErrorCode{
+    AUTH_NOT_FOUND(HttpStatus.UNAUTHORIZED,"등록되지 않은 계정입니다."),
 
-    AUTH_NOT_FOUND(HttpStatus.UNAUTHORIZED,"A-0001", "등록되지 않은 계정입니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일입니다."),
     ;
+    private static int iota = 1;
 
     private final HttpStatus httpStatus;
-    private final String errorCode;
     private final String errorMessage;
 
     public String getErrorName() {
         return this.name();
+    }
+
+    public String getErrorCode() {
+        String order = String.valueOf(this.ordinal() + 1);
+        return "A-" + "0".repeat(4-order.length()) + order;
     }
 }
