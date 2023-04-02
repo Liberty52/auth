3d87c8cf235f81f0fd5a83b6ec37e21fa920c979
parkcoldroad <coldstreet@naver.com>
2023-04-02 20:57:54 +0900

    feat: 로그인 상태유지 관한 로직 추가

--- src/main/java/com/liberty52/auth/global/jwt/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/liberty52/auth/global/jwt/JwtAuthenticationProcessingFilter.java
@@ -6,13 +6,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @RequiredArgsConstructor
 @Slf4j
