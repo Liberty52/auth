d0d8e7cedba3e51ac6a370ee059f90da8786da60
kyoohyun <kgh2120@naver.com>
2023-04-02 15:38:52 +0900

    CHORE : #7 관련 수정 & 프로필 별 도커파일 생성 & 빌드 에러 수정 & 로그파일 삭제

--- src/test/java/com/liberty52/auth/service/fake/FakeAuthRepository.java
+++ src/test/java/com/liberty52/auth/service/fake/FakeAuthRepository.java
@@ -27,12 +27,6 @@ public class FakeAuthRepository implements AuthRepository {
     public Optional<Auth> findByRefreshToken(String refreshToken) {
         return Optional.empty();
     }
-
-    @Override
-    public Optional<Auth> findByEmailAndPassword(String email, String password) {
-        return Optional.empty();
-    }
-
     @Override
     public void flush() {
 
