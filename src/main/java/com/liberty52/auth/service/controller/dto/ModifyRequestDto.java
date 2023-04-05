3b09347ea54cf42e22b123272c9e15eb1eb92338
parkcoldroad <coldstreet@naver.com>
2023-04-05 13:53:02 +0900

    feat: 회원 정보 수정 관련 dto 생성

---
+++ src/main/java/com/liberty52/auth/service/controller/dto/ModifyRequestDto.java
@@ -0,0 +1,8 @@
+package com.liberty52.auth.service.controller.dto;
+
+public class ModifyRequestDto {
+  private String originPassword;
+  private String updatePassword;
+  private String phoneNumber;
+  private String name;
+}
