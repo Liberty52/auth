554f7444db4b3150dba1a02f7d23e14b919c235e
hseungho <42243302+hseungho@users.noreply.github.com>
2023-04-13 16:56:35 +0900

    Issue 30 - 로그인 실패(비밀번호 오류) 케이스 (#37)
    * 생성되는 dummy 파일 ignore 추가
    
    * 비밀번호 오류 케이스에 대한 특정 예외 상황 추가

--- src/main/java/com/liberty52/auth/service/applicationservice/LoginServiceImpl.java
+++ src/main/java/com/liberty52/auth/service/applicationservice/LoginServiceImpl.java
@@ -1,6 +1,7 @@
 package com.liberty52.auth.service.applicationservice;
 
 import com.liberty52.auth.global.exception.external.AuthNotFoundException;
+import com.liberty52.auth.global.exception.external.AuthUnauthorizedException;
 import com.liberty52.auth.global.jwt.JwtService;
 import com.liberty52.auth.service.controller.dto.EmailLoginRequestDto;
 import com.liberty52.auth.service.controller.dto.LoginResponseDto;
@@ -26,7 +27,7 @@ public class LoginServiceImpl implements LoginService {
   public LoginResponseDto login(EmailLoginRequestDto dto, HttpServletResponse response) {
     Auth auth = authRepository.findByEmail(dto.getEmail()).orElseThrow(AuthNotFoundException::new);
     if (!encoder.matches(dto.getPassword(), auth.getPassword())) {
-      throw new AuthNotFoundException();
+      throw new AuthUnauthorizedException();
     }
     createTokenToResponse(response, auth);
     return LoginResponseDto.of(auth.getName(), auth.getProfileUrl(), auth.getRole());
