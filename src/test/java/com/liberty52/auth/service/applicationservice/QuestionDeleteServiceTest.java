30a23d5f3ffe871f1db16673c0cf75ded93dc598
ansm6358 <ansm6358@naver.com>
2023-04-26 21:32:17 +0900

    테스트와 관련없는 내용 삭제

--- src/test/java/com/liberty52/auth/service/applicationservice/QuestionDeleteServiceTest.java
+++ src/test/java/com/liberty52/auth/service/applicationservice/QuestionDeleteServiceTest.java
@@ -1,2 +1,54 @@
-package com.liberty52.auth.service.applicationservice;public class QuestionDeleteServiceTest {
+package com.liberty52.auth.service.applicationservice;
+
+import com.liberty52.auth.global.exception.external.NotYourQuestionException;
+import com.liberty52.auth.global.exception.external.QuestionNotFoundById;
+import com.liberty52.auth.service.controller.dto.QuestionCreateRequestDto;
+import com.liberty52.auth.service.entity.Question;
+import com.liberty52.auth.service.entity.QuestionStatus;
+import com.liberty52.auth.service.repository.QuestionRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class QuestionDeleteServiceTest {
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Autowired
+    QuestionDeleteService questionDeleteService;
+
+    String writerId = "testId";
+    String questionId;
+
+    @BeforeEach
+    void init() {
+        String title = "제목";
+        String content = "내용";
+        Question question = Question.create(title, content, writerId);
+        questionId = question.getId();
+        questionRepository.save(question);
+    }
+
+    @Test
+    void 문의추가() {
+
+        Question beforeQuestion = questionRepository.findById(questionId).orElseGet(null);
+        assertThat(beforeQuestion.getWriterId().equals(writerId));
+        Assertions.assertThrows(QuestionNotFoundById.class, () -> questionDeleteService.deleteQuestion(writerId, "err"));
+        Assertions.assertThrows(NotYourQuestionException.class, () -> questionDeleteService.deleteQuestion("err", questionId));
+
+        questionDeleteService.deleteQuestion(writerId, questionId);
+        Question afterQuestion = questionRepository.findById(questionId).orElse(null);
+        Assertions.assertNull(afterQuestion);
+    }
 }
