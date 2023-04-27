cca707d38c14a3b17538f4e92a71dfabffe66734
parkcoldroad <coldstreet@naver.com>
2023-04-27 16:16:16 +0900

    feat: 상세 문의 조회 컨트롤러 추가

--- src/main/java/com/liberty52/auth/service/controller/QuestionRetrieveController.java
+++ src/main/java/com/liberty52/auth/service/controller/QuestionRetrieveController.java
@@ -1,6 +1,7 @@
 package com.liberty52.auth.service.controller;
 
 import com.liberty52.auth.service.applicationservice.QuestionRetrieveService;
+import com.liberty52.auth.service.controller.dto.QuestionDetailResponseDto;
 import com.liberty52.auth.service.controller.dto.QuestionRetrieveResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -23,6 +25,12 @@ public class QuestionRetrieveController {
       @RequestParam(value = "page", defaultValue = "0") int pageNumber,
       @RequestParam(value = "size", defaultValue = "10") int size){
     return ResponseEntity.ok(questionRetrieveService.retrieveQuestions(writerId,pageNumber,size));
+  }
+  @GetMapping("/questions/{questionId}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<QuestionDetailResponseDto> retrieveQuestionDetail(@PathVariable("questionId") String questionId,
+      @RequestHeader(HttpHeaders.AUTHORIZATION) String writerId){
+    return ResponseEntity.ok(questionRetrieveService.retrieveQuestionDetail(questionId,writerId));
   }
 
 }
