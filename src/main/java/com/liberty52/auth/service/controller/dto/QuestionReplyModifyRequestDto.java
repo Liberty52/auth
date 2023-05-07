9fc563c42db34078c719a45dfbf99ab5f9391ace
JIN-YONG LEE <74762475+jinlee0@users.noreply.github.com>
2023-05-07 13:00:52 +0900

    fix: add NoArgsConstructor (#57)

--- src/main/java/com/liberty52/auth/service/controller/dto/QuestionReplyModifyRequestDto.java
+++ src/main/java/com/liberty52/auth/service/controller/dto/QuestionReplyModifyRequestDto.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class QuestionReplyModifyRequestDto {
     @NotEmpty
     @Size(max = 10000)
