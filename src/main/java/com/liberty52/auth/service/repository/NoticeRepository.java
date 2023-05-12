e3ccd93699c47ca82c172176d21d4cfdd0b87036
hseungho <hsh47607@naver.com>
2023-05-13 00:09:01 +0900

    공지사항 조회 API - 리스트 & 상세정보 조회

--- src/main/java/com/liberty52/auth/service/repository/NoticeRepository.java
+++ src/main/java/com/liberty52/auth/service/repository/NoticeRepository.java
@@ -2,7 +2,13 @@ package com.liberty52.auth.service.repository;
 
 import com.liberty52.auth.service.entity.Notice;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, String> {
+
+    Page<Notice> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
 }
