96b48fe65a1b424be86dbbfc0002ce6d96e2c2cf
hseungho <hsh47607@naver.com>
2023-05-13 01:35:35 +0900

    ISSUE-79 일부 로그인 기능 Refactoring
    refactor : Login & JWT Service 일부 기능 역할 위임.
    chore: 관리자 더미 데이터 3개 추가.

--- src/main/java/com/liberty52/auth/global/jwt/JwtService.java
+++ src/main/java/com/liberty52/auth/global/jwt/JwtService.java
@@ -2,6 +2,8 @@ package com.liberty52.auth.global.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.liberty52.auth.global.exception.external.AuthNotFoundException;
+import com.liberty52.auth.service.entity.Auth;
 import com.liberty52.auth.service.entity.Role;
 import com.liberty52.auth.service.repository.AuthRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,125 +21,136 @@ import java.util.Optional;
 @Slf4j
 public class JwtService {
 
-  @Value("${jwt.secretKey}")
-  private String SECRET_KEY;
+    @Value("${jwt.secretKey}")
+    private String SECRET_KEY;
 
-  @Value("${jwt.access.expiration}")
-  private Long ACCESS_TOKEN_EXPIRATION;
+    @Value("${jwt.access.expiration}")
+    private Long ACCESS_TOKEN_EXPIRATION;
 
-  @Value("${jwt.refresh.expiration}")
-  private Long REFRESH_TOKEN_EXPIRATION;
+    @Value("${jwt.refresh.expiration}")
+    private Long REFRESH_TOKEN_EXPIRATION;
 
-  @Value("${jwt.access.header}")
-  private String ACCESS_HEADER;
+    @Value("${jwt.access.header}")
+    private String ACCESS_HEADER;
 
-  @Value("${jwt.refresh.header}")
-  private String REFRESH_HEADER;
+    @Value("${jwt.refresh.header}")
+    private String REFRESH_HEADER;
 
-  @Value("${jwt.value.prefix.bearer}")
-  private String PREFIX_BEARER;
+    @Value("${jwt.value.prefix.bearer}")
+    private String PREFIX_BEARER;
 
-  @Value("${jwt.value.prefix.basic}")
-  private String PREFIX_BASIC;
+    @Value("${jwt.value.prefix.basic}")
+    private String PREFIX_BASIC;
 
-  @Value("${jwt.value.claim.auth-id}")
-  private String CLAIM_AUTH_ID;
+    @Value("${jwt.value.claim.auth-id}")
+    private String CLAIM_AUTH_ID;
 
-  @Value("${jwt.value.claim.role}")
-  private String CLAIM_ROLE;
+    @Value("${jwt.value.claim.role}")
+    private String CLAIM_ROLE;
 
-  private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
-  private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
 
-  private final AuthRepository authRepository;
+    private final AuthRepository authRepository;
 
-  public String createAccessToken(String id, Role role) {
-    Date now = new Date();
-    return JWT.create()
-            .withSubject(ACCESS_TOKEN_SUBJECT)
-            .withExpiresAt(new Date(now.getTime() + ACCESS_TOKEN_EXPIRATION))
-            .withClaim(CLAIM_AUTH_ID, id)
-            .withClaim(CLAIM_ROLE, role.getKey())
-            .sign(Algorithm.HMAC512(SECRET_KEY));
-  }
-
-  public String createRefreshToken() {
-    Date now = new Date();
-    return JWT.create()
-        .withSubject(REFRESH_TOKEN_SUBJECT)
-        .withExpiresAt(new Date(now.getTime() + REFRESH_TOKEN_EXPIRATION))
-        .sign(Algorithm.HMAC512(SECRET_KEY));
-  }
-
-  public void sendAccessToken(HttpServletResponse response, String accessToken) {
-    response.setStatus(HttpServletResponse.SC_OK);
-
-    response.setHeader(ACCESS_HEADER, accessToken);
-    log.debug("재발급된 Access Token : {}", accessToken);
-  }
-
-  public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
-    response.setStatus(HttpServletResponse.SC_OK);
-
-    setAccessTokenHeader(response, accessToken);
-    setRefreshTokenHeader(response, refreshToken);
-    log.debug("Access Token, Refresh Token 헤더 설정 완료");
-  }
-
-  public Optional<String> extractRefreshToken(HttpServletRequest request) {
-    return Optional.ofNullable(request.getHeader(REFRESH_HEADER))
-        .filter(refreshToken -> refreshToken.startsWith(PREFIX_BEARER))
-        .map(refreshToken -> refreshToken.replace(PREFIX_BEARER, ""));
-  }
-
-  public Optional<String> extractAccessToken(HttpServletRequest request) {
-    return Optional.ofNullable(request.getHeader(ACCESS_HEADER))
-        .filter(token -> token.startsWith(PREFIX_BEARER))
-        .map(token -> token.replace(PREFIX_BEARER, ""));
-  }
-
-  public Optional<String> extractAuthId(String accessToken) {
-    try {
-      Optional<String> result = Optional.ofNullable(JWT.require(Algorithm.HMAC512(SECRET_KEY))
-          .build()
-          .verify(accessToken)
-          .getClaim(CLAIM_AUTH_ID)
-          .asString());
-
-      return result;
-    } catch (Exception e) {
-      log.error("액세스 토큰이 유효하지 않습니다.");
-      return Optional.empty();
+    public String createAccessToken(String id, Role role) {
+        Date now = new Date();
+        return JWT.create()
+                .withSubject(ACCESS_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(now.getTime() + ACCESS_TOKEN_EXPIRATION))
+                .withClaim(CLAIM_AUTH_ID, id)
+                .withClaim(CLAIM_ROLE, role.getKey())
+                .sign(Algorithm.HMAC512(SECRET_KEY));
     }
-  }
 
-  public void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
-    response.setHeader(ACCESS_HEADER, accessToken);
-  }
-
-  public void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
-    response.setHeader(REFRESH_HEADER, refreshToken);
-  }
-
-  public void updateRefreshToken(String email, String updateRefreshToken) {
-    authRepository.findByEmail(email)
-        .ifPresentOrElse(
-            user -> {
-              user.updateRefreshToken(updateRefreshToken);
-              authRepository.save(user);
-            },
-            () -> new Exception("일치하는 회원이 없습니다.")
-        );
-  }
-
-  public boolean isTokenValid(String token) {
-    try {
-      JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(token);
-      return true;
-    } catch (Exception e) {
-      log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
-      return false;
+    public String createRefreshToken() {
+        Date now = new Date();
+        return JWT.create()
+                .withSubject(REFRESH_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(now.getTime() + REFRESH_TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SECRET_KEY));
     }
-  }
+
+    public void sendAccessToken(HttpServletResponse response, String accessToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.setHeader(ACCESS_HEADER, accessToken);
+        log.debug("재발급된 Access Token : {}", accessToken);
+    }
+
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken);
+        setRefreshTokenHeader(response, refreshToken);
+        log.debug("Access Token, Refresh Token 헤더 설정 완료");
+    }
+
+    public Optional<String> extractRefreshToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(REFRESH_HEADER))
+                .filter(refreshToken -> refreshToken.startsWith(PREFIX_BEARER))
+                .map(refreshToken -> refreshToken.replace(PREFIX_BEARER, ""));
+    }
+
+    public Optional<String> extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(ACCESS_HEADER))
+                .filter(token -> token.startsWith(PREFIX_BEARER))
+                .map(token -> token.replace(PREFIX_BEARER, ""));
+    }
+
+    public Optional<String> extractAuthId(String accessToken) {
+        try {
+            Optional<String> result = Optional.ofNullable(JWT.require(Algorithm.HMAC512(SECRET_KEY))
+                    .build()
+                    .verify(accessToken)
+                    .getClaim(CLAIM_AUTH_ID)
+                    .asString());
+
+            return result;
+        } catch (Exception e) {
+            log.error("액세스 토큰이 유효하지 않습니다.");
+            return Optional.empty();
+        }
+    }
+
+    public void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
+        response.setHeader(ACCESS_HEADER, accessToken);
+    }
+
+    public void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
+        response.setHeader(REFRESH_HEADER, refreshToken);
+    }
+
+    public void updateRefreshToken(String email, String updateRefreshToken) {
+        authRepository.findByEmail(email)
+            .ifPresentOrElse(
+                user -> {
+                    user.updateRefreshToken(updateRefreshToken);
+                    authRepository.save(user);
+                }, AuthNotFoundException::new
+            );
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(token);
+            return true;
+        } catch (Exception e) {
+            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public void createTokenAndAddHeaders(Auth auth, boolean isAutoLogin, HttpServletResponse response) {
+        final String access = "access"; // 위 상수와 일치시키고 싶지만, FE와 맞춰야 하기 때문에 일단 유지. 추후 수정 예정.
+        final String refresh = "refresh";
+
+        String accessToken = this.createAccessToken(auth.getId(), auth.getRole());
+        response.addHeader(access, PREFIX_BEARER + accessToken);
+        if (isAutoLogin) {
+            String refreshToken = this.createRefreshToken();
+            response.addHeader(refresh, refreshToken);
+        }
+    }
 }
 
--- src/main/java/com/liberty52/auth/service/applicationservice/LoginServiceImpl.java
+++ src/main/java/com/liberty52/auth/service/applicationservice/LoginServiceImpl.java
@@ -9,7 +9,6 @@ import com.liberty52.auth.service.controller.dto.LoginResponseDto;
 import com.liberty52.auth.service.entity.Auth;
 import com.liberty52.auth.service.repository.AuthRepository;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -31,7 +30,7 @@ public class LoginServiceImpl implements LoginService {
     if (!encoder.matches(dto.getPassword(), auth.getPassword())) {
       throw new AuthUnauthorizedException();
     }
-    createTokenToResponse(response, auth);
+    jwtService.createTokenAndAddHeaders(auth, true, response);
     return LoginResponseDto.of(auth.getName(), auth.getProfileUrl(), auth.getRole());
   }
 
@@ -39,15 +38,7 @@ public class LoginServiceImpl implements LoginService {
   public void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
     Auth auth = authRepository.findByRefreshToken(refreshToken)
         .orElseThrow(InvalidTokenException::new);
-    createTokenToResponse(response, auth);
+    jwtService.createTokenAndAddHeaders(auth, true, response);
   }
 
-  private void createTokenToResponse(HttpServletResponse response, Auth auth) {
-    String accessToken = jwtService.createAccessToken(auth.getId(), auth.getRole());
-    String refreshToken = jwtService.createRefreshToken();
-    auth.updateRefreshToken(refreshToken);
-
-    response.addHeader("access", "Bearer " + accessToken);
-    response.addHeader("refresh", refreshToken);
-  }
 }
