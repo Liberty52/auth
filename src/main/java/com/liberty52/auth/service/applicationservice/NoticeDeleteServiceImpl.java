45e2c24b46f537c4ca5de5e9f72dba9379eedeb8
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-05-12 16:46:50 +0900

    LIB-214 공지사항 삭제 (#76)
    * feat: 관리자 권한 util 추가
    
    * feat: 공지사항 삭제 컨트롤러 추가
    
    * feat: 공지사항 삭제 서비스 추가
    
    * test: 공지사항 삭제 테스트 추가
    
    * refactor: 공지사항 엔드포인트 수정
    
    * fix: Change ok response to noContent response

---
+++ src/main/java/com/liberty52/auth/service/applicationservice/NoticeDeleteServiceImpl.java
@@ -0,0 +1,23 @@
+package com.liberty52.auth.service.applicationservice;
+
+import com.liberty52.auth.global.exception.external.ResourceNotFoundException;
+import com.liberty52.auth.global.utils.AdminRoleUtils;
+import com.liberty52.auth.service.entity.Notice;
+import com.liberty52.auth.service.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class NoticeDeleteServiceImpl implements NoticeDeleteService{
+  private final NoticeRepository noticeRepository;
+  @Override
+  public void deleteNotice(String role, String noticeId) {
+    AdminRoleUtils.checkRole(role);
+    Notice notice = noticeRepository.findById(noticeId)
+        .orElseThrow(() -> new ResourceNotFoundException("notice", "id", noticeId));
+    noticeRepository.delete(notice);
+  }
+}
