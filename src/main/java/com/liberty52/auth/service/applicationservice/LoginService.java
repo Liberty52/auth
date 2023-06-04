c671b8290348666434b04913f48c261b35d79081
parkcoldroad <coldstreet@naver.com>
2023-06-04 22:56:37 +0900

    fix: 로그인 상태유지 관련 이슈 해결

--- src/main/java/com/liberty52/auth/service/applicationservice/LoginService.java
+++ src/main/java/com/liberty52/auth/service/applicationservice/LoginService.java
@@ -1,13 +1,12 @@
 package com.liberty52.auth.service.applicationservice;
 
-import com.liberty52.auth.service.controller.dto.EmailLoginRequestDto;
+import com.liberty52.auth.service.controller.dto.LoginRequestDto;
 import com.liberty52.auth.service.controller.dto.LoginResponseDto;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
 public interface LoginService {
-    LoginResponseDto login(EmailLoginRequestDto dto, HttpServletResponse response);
+    LoginResponseDto login(LoginRequestDto dto, HttpServletResponse response);
 
     void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken);
 }
--- src/main/java/com/liberty52/auth/service/controller/dto/LoginRequestDto.java
+++ src/main/java/com/liberty52/auth/service/controller/dto/LoginRequestDto.java
@@ -1,12 +1,15 @@
 package com.liberty52.auth.service.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
-public class EmailLoginRequestDto {
+public class LoginRequestDto {
     @NotBlank
     private String email;
     @NotBlank
     private String password;
+    @NotNull
+    private boolean check;
 }
