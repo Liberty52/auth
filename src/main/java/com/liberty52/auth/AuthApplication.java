f026684d29988deb78857f39e5cba2c5911db539
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-04 11:20:08 +0900

    chore: Enhance AuthApplication with logging and server startup information

--- src/main/java/com/liberty52/auth/AuthApplication.java
+++ src/main/java/com/liberty52/auth/AuthApplication.java
@@ -1,16 +1,70 @@
 package com.liberty52.auth;
 
+import com.liberty52.auth.global.config.CRLFLogConverter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableDiscoveryClient
+@Slf4j
 public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+		SpringApplication app = new SpringApplication(AuthApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logApplicationStartup(env);
+	}
+
+	private static void logApplicationStartup(Environment env) {
+		String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+		String serverPort = env.getProperty("server.port");
+		String contextPath = Optional
+				.ofNullable(env.getProperty("server.servlet.context-path"))
+				.filter(StringUtils::isNotBlank)
+				.orElse("/");
+		String hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			log.warn("The host name could not be determined, using `localhost` as fallback");
+		}
+		log.info(
+				CRLFLogConverter.CRLF_SAFE_MARKER,
+				"\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\t{}://localhost:{}{}\n\t" +
+						"External: \t{}://{}:{}{}\n\t" +
+						"Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				protocol,
+				serverPort,
+				contextPath,
+				protocol,
+				hostAddress,
+				serverPort,
+				contextPath,
+				env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
+		);
+
+		String configServerStatus = env.getProperty("configserver.status");
+		if (configServerStatus == null) {
+			configServerStatus = "Not found or not setup for this application";
+		}
+		log.info(
+				CRLFLogConverter.CRLF_SAFE_MARKER,
+				"\n----------------------------------------------------------\n\t" +
+						"Config Server: \t{}\n----------------------------------------------------------",
+				configServerStatus
+		);
 	}
 
 }
